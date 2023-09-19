261a3eb1598fec5488991bff3760ad6d76335d63
Richard Marrone <RichardMarrone54@gmail.com>
2023-09-18 21:09:04 -0400

    Add / Save / Delete / Update Logic complete

--- popup.js
+++ popup.js
@@ -1,6 +1,20 @@
-// elements
-var table = document.querySelector('.param-table');
-const paramButton = document.querySelector('.add-param-btn');
+/*
+TODO:
+    - truncute url in preview or start wrapping at lower width
+    - error handling
+    - Make CSS look nice
+Nice to have:
+    - refactoring
+    - settings pane
+    - better readme
+*/
+
+// Globals
+const table = document.querySelector('.param-table');
+const addParamButton = document.querySelector('.add-param-btn');
+const urlDisplay = document.querySelector('.current-url');
+const goButton = document.querySelector('.go-btn');
+let globalUrlObject = null;
 
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
@@ -9,38 +23,74 @@ async function getCurrentTab() {
     return tab;
   }
 
+function renderUrl() {
+    urlDisplay.innerHTML = globalUrlObject ? globalUrlObject.toString(): '';
+}
+
 function generateTableRow(param='', value='') {
     /*
     create row of this form and append to global parameters table
-    ----------------------------
-    - param | value | (Remove) -
-    ----------------------------
+    --------------------------------------------
+    - param | value | (Remove) | (Edit / Save) -
+    --------------------------------------------
     */
     const row = document.createElement('tr');   
 
-    const paramColumn = document.createElement('td');
-    paramColumn.setAttribute('contenteditable','true')
-    const valueColumn = document.createElement('td');
-    valueColumn.setAttribute('contenteditable','true')
+    row.innerHTML =`
+    <tr>
+        <td class="param">${param}</td>
+        <td class="value">${value}</td>
+        <td class="delete-button">
+            <button>Remove</button>
+        </td>
+        <td class="save-button" style="display:none;">
+            <button>Save</button>
+        </td>
+        <td class="edit-button">
+            <button>Edit</button>
+        </td>
+    </tr>
+    `;
 
-    const paramText = document.createTextNode(param);
-    const valueText = document.createTextNode(value);
+    const thisParam = row.querySelector('.param');
+    const thisValue = row.querySelector('.value');
 
-    paramColumn.appendChild(paramText);
-    valueColumn.appendChild(valueText);
-    row.appendChild(paramColumn);
-    row.appendChild(valueColumn);
+    thisParam.addEventListener('input', () => {
+        console.log(thisParam.textContent);
+    })
 
-    const deleteButtonCell = document.createElement('td');
-    const deleteButton = document.createElement('button');
-    const deleteText = document.createTextNode('Remove');
+    const deleteButton = row.querySelector('.delete-button');
+    const editButton = row.querySelector('.edit-button');
+    const saveButton = row.querySelector('.save-button');
 
     deleteButton.addEventListener('click', () => {
+        if (globalUrlObject) {
+            // We send value as well for the cases where there are params with the same name
+            globalUrlObject.searchParams.delete(thisParam.textContent, thisValue.textContent);
+        }
+        renderUrl();
         row.remove();
-    })
-    deleteButton.appendChild(deleteText);
-    deleteButtonCell.appendChild(deleteButton);
-    row.appendChild(deleteButtonCell);
+    });
+
+    editButton.addEventListener('click', () => {
+        saveButton.style.display = '';
+        thisParam.setAttribute('contenteditable', 'true');
+        thisValue.setAttribute('contenteditable', 'true');
+        editButton.style.display = 'none';
+        renderUrl();
+    });
+
+    saveButton.addEventListener('click', () => {
+        editButton.style.display = '';
+        thisParam.setAttribute('contenteditable', 'false');
+        thisValue.setAttribute('contenteditable', 'false');
+        if (globalUrlObject && thisParam.textContent) {
+            globalUrlObject.searchParams.append(thisParam.textContent, thisValue.textContent);
+        }
+        saveButton.style.display = 'none';
+        renderUrl();
+    });
+
     return row;
 }
 
@@ -51,25 +101,22 @@ function generateTable(params) {
     }
 }
 
-getCurrentTab().then(res => {
-    console.log(res)
-    const urlHtml = document.querySelector('.current-url');
-    urlHtml.innerHTML = res.url;
-
-    const urlObject = new URL(res.url);
-    generateTable(urlObject.searchParams);
-})
-
-paramButton.addEventListener('click', () => {
+addParamButton.addEventListener('click', () => {
     const row = generateTableRow();
     table.appendChild(row);
 });
 
+getCurrentTab().then(res => {
+    globalUrlObject = new URL(res.url);
+    renderUrl();
+    const currentSearchParams = globalUrlObject.searchParams;
+    generateTable(currentSearchParams);
+})
 
-// const myButton = document.querySelector('.test-btn');
-// myButton.addEventListener('click', () => {
-//     getCurrentTab().then(tab => {
-//         chrome.tabs.update(undefined, { url: 'https://google.com' }); // update and navigate
-//     })
-// });
+goButton.addEventListener('click', () => {
+    const newUrl = globalUrlObject.toString();
+    getCurrentTab().then(tab => {
+        chrome.tabs.update(undefined, { url: newUrl }); // update and navigate
+    })
+});
 
