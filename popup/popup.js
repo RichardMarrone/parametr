096c68cac0b3ff2063cffe297edbb66527158a2f
Richard Marrone <RichardMarrone54@gmail.com>
2023-09-19 19:38:06 -0400

    reformat folder structure. Save/Edit bug fixes

--- popup/popup.js
+++ popup/popup.js
@@ -10,7 +10,7 @@ Nice to have:
 */
 
 // Globals
-const table = document.querySelector('.param-table');
+const table = document.querySelector('.param-table tbody');
 const addParamButton = document.querySelector('.add-param-btn');
 const urlDisplay = document.querySelector('.current-url');
 const goButton = document.querySelector('.go-btn');
@@ -30,35 +30,38 @@ function renderUrl() {
 function generateTableRow(param='', value='') {
     /*
     create row of this form and append to global parameters table
+    if we pass param and value, it was parsed from the current url
+    so we display those values. If they are empty we know the user
+    has just added a new row.
     --------------------------------------------
     - param | value | (Remove) | (Edit / Save) -
     --------------------------------------------
     */
-    const row = document.createElement('tr');   
+    const row = document.createElement('tr');
+    const userAddedRow = !param && !value;
 
     row.innerHTML =`
     <tr>
-        <td class="param">${param}</td>
-        <td class="value">${value}</td>
+        <td>
+            <div ${userAddedRow ? `contenteditable='true'`: `history='${param}'`} class="param">${param}</div>
+        </td>
+        <td>
+            <div ${userAddedRow ? `contenteditable='true'`: `history='${value}'`} class="value">${value}</div>
+        </td>
+        <td class="edit-button" ${userAddedRow ? `style="display:none;"`: ''}>
+            <button>Edit</button>
+        </td>
+        <td class="save-button" ${userAddedRow ? '': `style="display:none;"`}>
+            <button>Save</button>
+        </td>        
         <td class="delete-button">
             <button>Remove</button>
-        </td>
-        <td class="save-button" style="display:none;">
-            <button>Save</button>
-        </td>
-        <td class="edit-button">
-            <button>Edit</button>
         </td>
     </tr>
     `;
 
     const thisParam = row.querySelector('.param');
     const thisValue = row.querySelector('.value');
-
-    thisParam.addEventListener('input', () => {
-        console.log(thisParam.textContent);
-    })
-
     const deleteButton = row.querySelector('.delete-button');
     const editButton = row.querySelector('.edit-button');
     const saveButton = row.querySelector('.save-button');
@@ -71,7 +74,7 @@ function generateTableRow(param='', value='') {
         renderUrl();
         row.remove();
     });
-
+    
     editButton.addEventListener('click', () => {
         saveButton.style.display = '';
         thisParam.setAttribute('contenteditable', 'true');
@@ -81,12 +84,29 @@ function generateTableRow(param='', value='') {
     });
 
     saveButton.addEventListener('click', () => {
+        const thisParamBefore = thisParam.getAttribute('history') ?? '';
+        const thisValueBefore = thisValue.getAttribute('history') ?? '';
+        console.log(`Param history: ${thisParamBefore} value history: ${thisValueBefore}`);
         editButton.style.display = '';
         thisParam.setAttribute('contenteditable', 'false');
         thisValue.setAttribute('contenteditable', 'false');
+
+        if (thisParam.textContent != thisParamBefore || thisValue.textContent != thisValueBefore) {
+            // if we changed an existing value, delete the old
+            if (globalUrlObject) {
+                globalUrlObject.searchParams.delete(thisParamBefore, thisValueBefore);
+            }
+        }
+
         if (globalUrlObject && thisParam.textContent) {
+            // we can have blank values but we at least need param
             globalUrlObject.searchParams.append(thisParam.textContent, thisValue.textContent);
         }
+
+        // update history
+        thisParam.setAttribute('history', thisParam.textContent);
+        thisValue.setAttribute('history', thisValue.textContent);
+
         saveButton.style.display = 'none';
         renderUrl();
     });
@@ -116,7 +136,8 @@ getCurrentTab().then(res => {
 goButton.addEventListener('click', () => {
     const newUrl = globalUrlObject.toString();
     getCurrentTab().then(tab => {
-        chrome.tabs.update(undefined, { url: newUrl }); // update and navigate
+        chrome.tabs.create({url: newUrl});
+        // chrome.tabs.update(undefined, { url: newUrl }); // update and navigate
     })
 });
 
